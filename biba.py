c723f3c3c7ed3a683bbbb4f79fd59dda0bb4d8bd
EstateEterna <sokolikdara76@gmail.com>
2023-10-20 15:34:53 +0300

    add biba py

---
+++ biba.py
@@ -0,0 +1,5 @@
+def main():
+    name = "aaaaaaaaaaaas"
+    print("Hello, {name}")
+
+main()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main():
     name = "Dara"
-    print("Hello, {name}")
+    for i in range(10):
+        print("Hello, {name}")
 
 main()
